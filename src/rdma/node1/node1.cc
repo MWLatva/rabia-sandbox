1fa3726765d540603236b4fcbf2365cfcc122833
mlatva <mwl325@lehigh.edu>
2024-04-28 15:36:21 -0400

    linking remus

--- src/rdma/node1/node1.cc
+++ src/rdma/node1/node1.cc
@@ -8,42 +8,37 @@
 #include <unistd.h> 
 #include <google/protobuf/message_lite.h> 
 #include "../../Rabia_build/message.pb.h"
-//using namespace google::protobuf;
-  
-//using namespace std; 
-// namespace google::protobuf;
+
+#include <remus/rdma/connection_map.h>
+#include <remus/util/status.h>
+
+#include <atomic>
+#include <thread>
+#include <unordered_map>
+
+using namespace std;
+using namespace remus::rdma::internal;
+using namespace remus::util;
+using namespace google::protobuf;
   
 int main() 
 { 
-  // creating socket 
-  int serverSocket = socket(AF_INET, SOCK_STREAM, 0); 
+ 
+  // make the connection object
 
-  // specifying the address 
-  sockaddr_in serverAddress; 
-  serverAddress.sin_family = AF_INET; 
-  serverAddress.sin_port = htons(8080); 
-  serverAddress.sin_addr.s_addr = INADDR_ANY; 
 
-  // binding socket. 
-  bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)); 
+  Connection* sender;
+  Connection* receiver;
 
   message::Msg sample_message;
 
-  // listening to the assigned socket 
-  listen(serverSocket, 5); 
+  /// Receive the result
+  StatusVal<Msg> maybe_result = sender->channel()->Deliver<Msg>();
+  lock_table_client[target_id]->unlock();
+  REMUS_ASSERT(maybe_result.status.t == Ok, "Cannot get result");
+  Msg result = maybe_result.val.value();
 
-  // accepting connection request 
-  int clientSocket = accept(serverSocket, nullptr, nullptr); 
-
-  // recieving data 
-  char buffer[1024] = { 0 }; 
-  recv(clientSocket, buffer, sizeof(buffer), 0); 
-  std::string data(buffer);
-  sample_message.ParseFromString(buffer);//couldnt figure out how to get thsi linked :(
-  std::cout << "Message from client recieved: \n" << sample_message.value() << std::endl; 
-
-  // closing the socket. 
-  close(serverSocket); 
+  std::cout << "Message from client recieved: \n" << result.value() << std::endl; 
 
   return 0; 
 }
--- src/rdma/node2/node2.cc
+++ src/rdma/node2/node2.cc
@@ -1,26 +1,73 @@
-// https://www.geeksforgeeks.org/socket-programming-in-cpp/
-// C++ program to illustrate the client application in the 
-// socket programming 
 #include <cstring> 
 #include <iostream> 
 #include <netinet/in.h> 
 #include <sys/socket.h> 
 #include <unistd.h> 
 #include "../../Rabia_build/message.pb.h"
+
+#include <remus/rdma/connection_map.h>
+#include <remus/util/status.h>
+
+#include <atomic>
+#include <thread>
+#include <unordered_map>
+
+using namespace std;
+using namespace remus::rdma::internal;
+using namespace remus::util;
+using namespace google::protobuf;
   
-int main() 
-{ 
-    // creating socket 
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
-  
-    // specifying address 
-    sockaddr_in serverAddress; 
-    serverAddress.sin_family = AF_INET; 
-    serverAddress.sin_port = htons(8080); 
-    serverAddress.sin_addr.s_addr = INADDR_ANY; 
+
+struct Peer {
+  const uint16_t id = 0;          // A unique Id for the peer
+  const std::string address = ""; // The public address of the peer
+  const uint16_t port = 0;        // The port on which the peer listens
+
+  /// Construct a Peer
+  ///
+  /// TODO: We probably don't need a constructor since this is so trivial
+  ///
+  /// @param id       The Id for the peer (default 0)
+  /// @param address  The ip address as a string (default "")
+  /// @param port     The port (default 0)
+  Peer(uint16_t id = 0, std::string address = "", uint16_t port = 0)
+      : id(id), address(address), port(port) {}
+};
+
+int main() {      
+    //thanks Ethan!
+    ROME_INIT_LOG();
+    ROME_INFO("CREATING PEER");
+    //create a peer
+    std::string node_name = "node0";
+    Peer self_peer = Peer(0, node_name, 18000);
+    Peer other_peer = Peer(1, "note1", 18001);
+
+    ConnectionManager* cm = new ConnectionManager(self_peer.id);
+
+    //start a connection manager on the 'self' peer
+    auto status = cm->Start(self_peer.address, self_peer.port);
+    RETURN_STATUSVAL_FROM_ERROR(status);
+
+    // connect self peer to cm
+    auto connected = cm->Connect(self_peer.id, self_peer.address, self_peer.port);
+    while (connected.status.t == Unavailable) {
+        connected = cm->Connect(self_peer.id, self_peer.address, self_peer.port);
+    }
+    RETURN_STATUSVAL_FROM_ERROR(connected.status);
+    ROME_INFO("Init done with {}", self_peer.address);
+
+    //connect other peer to cm
+    auto connected = cm->Connect(other_peer.id, other_peer.address, other_peer.port);
+    while (connected.status.t == Unavailable) {
+        connected = cm->Connect(other_peer.id, other_peer.address, other_peer.port);
+    }
+    RETURN_STATUSVAL_FROM_ERROR(connected.status);
+    ROME_INFO("Init done with {}", other_peer.address);
+
+    //__________________
 
     message::Msg sample_message;
-    //message::ConsensusObj consObj;
 
     sample_message.set_type(message::MsgType::Proposal);
     sample_message.set_phase(1);
@@ -28,18 +75,15 @@ int main()
     //sample_message.set_allocated_obj(consObj);
 
   
-    // sending connection request 
-    connect(clientSocket, (struct sockaddr*)&serverAddress, 
-            sizeof(serverAddress)); 
-  
-    // sending data 
-    std::string message;
-    sample_message.SerializeToString(&message); //protobuf serialise
-    send(clientSocket, message.c_str(), strlen(message.c_str()), 0); 
+    // // sending data 
+    // std::string message;
+    // sample_message.SerializeToString(&message); //protobuf serialise
+
+    auto conn = cm->GetConnection(1);
+    Status stat = conn.val.value()->channel()->Send(sample_message);
+    REMUS_ASSERT(stat.t == Ok, "Operation failed"); 
     
   
-    // closing socket 
-    close(clientSocket); 
   
     return 0; 
 }
